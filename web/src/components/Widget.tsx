00cf92974883efb69ba30946b744d40db5254172
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-05-28 02:51:58 -0300

    Widget Acessibility/Tailwindcss

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,21 +1,15 @@
 import { ChatTeardropDots } from "phosphor-react"
-import { useState } from "react"
-
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState<boolean>(false);
-
-    const handleWidgetOpen = () => {
-        setIsWidgetOpen(!isWidgetOpen);
-    }
-
+    
     return(
-        <div className="absolute bottom-5 right-5">
-            { isWidgetOpen &&
-            <p>Hello Jesus!</p>
+        <Popover className="absolute bottom-5 right-5">
+           
+            <Popover.Panel>Hello Jesus!</Popover.Panel>
             
-        }
-            <button onClick={handleWidgetOpen}
+       
+            <Popover.Button 
                 className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6" />
 
@@ -23,7 +17,7 @@ export function Widget() {
                 <span className="pl-2"></span>
                 Feedback
             </span>
-        </button>
-        </div>
+        </Popover.Button>
+        </Popover>
     )
 }
